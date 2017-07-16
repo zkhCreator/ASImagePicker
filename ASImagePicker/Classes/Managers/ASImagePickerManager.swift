48622c1c4f26eaf2560db45583221d38720931f6
zkhCreator <zkh90644@gmail.com>
2017-07-16 11:23:15 +0800

    add Manager, mdoel

---
+++ ASImagePicker/Classes/Managers/ASImagePickerManager.swift
@@ -0,0 +1,16 @@
+//
+//  ASImagePickerManager.swift
+//  Pods
+//
+//  Created by 章凯华 on 16/07/2017.
+//
+//
+
+import UIKit
+
+class ASImagePickerManager: NSObject {
+    static let shared = ASImagePickerManager()
+    
+    
+    
+}
